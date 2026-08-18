99d004637944ff198b4b5e7d21e267ac9a4973ce
MD AMIRUL HAQUE <haqueamir70@gmail.com>
2026-08-18 16:10:35 +0300

    Added Terraform variables configuration

---
+++ terraform/terraform/variables.tf
@@ -0,0 +1,16 @@
+variable "location" {
+
+  description = "Azure region"
+
+  default = "East US"
+
+}
+
+
+variable "environment" {
+
+  description = "Deployment environment"
+
+  default = "production"
+
+}
